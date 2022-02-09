abd66acd35062e905829cb6f8f5e8d7501bf177b
Michael Novajosky <mikenovajosky@yahoo.com>
2022-02-09 15:31:26 -0800

    add many to many association

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
     ('D', 'Dinner')
 )
+
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
 
 # Create your models here.
 class Dog(models.Model):
@@ -16,12 +28,16 @@ class Dog(models.Model):
     age = models.IntegerField()
     #there will be a feeding set related-manager 
     # used to access a cats feedings
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS) 
 
 class Feeding(models.Model):
     date = models.DateField('Feeding Date')
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from dataclasses import fields
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
 from .forms import FeedingForm
 
 # Define the home view
@@ -17,15 +18,22 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  # instantiate FeedingForm to be rendered within
+  # the detail.html template
+  id_list = dog.toys.all().values_list('id')
+  #query for toys whos ids are not in the list using exclude
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in=id_list)
   feeding_form = FeedingForm()
   return render(request, 'dogs/detail.html', {
-    'dog': dog, 'feeding_form': feeding_form 
-    })
+    'dog': dog,
+    'feeding_form': feeding_form,
+    'toys': toys_dog_doesnt_have
+  })
 
 class DogCreate(CreateView):
     model = Dog
-    fields = '__all__'
-    success_url = '/dogs/'
+    # fields = '__all__'
+    fields = ['name', 'breed', 'description', 'age']
 
 class DogUpdate(UpdateView):
   model = Dog
@@ -46,4 +54,31 @@ def add_feeding(request, dog_id):
     new_feeding = form.save(commit=False)
     new_feeding.dog_id = dog_id
     new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+def un_assoc_toy(request, dog_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+  Dog.objects.get(id=dog_id).toys.remove(toy_id)
   return redirect('detail', dog_id=dog_id)
